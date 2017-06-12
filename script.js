498797b8e2a66bd775f5bc5aa841511047a3054e
Dennis Dang <dangggdennis.com>
2017-06-12 15:14:31 -0700

    Calculator v0.1 - Dennis Dang

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+
+Add a digit to the display.
+Put a decimal point on the display (if not there already)
+Put an exponent ('e') on the display.
+Change the sign to + if - and minus if plus.
+Clear and All Clear (C and AC)
+Perform operations * / + and -
+Calculate (on pressing the = button)
+
+*/
+
+function Calculator(num) {
+	this.addition = function(){
+
+	}
+}
